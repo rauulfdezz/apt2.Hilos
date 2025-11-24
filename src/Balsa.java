7a0cb8ea8b9cbc1164ef102795abe25c6a83033c
Raul <raul@gmail.com>
2025-11-24 13:02:52 +0100

    He añadido un cosntructor nuevo con capacidad, tiempo y nombre para construir las balssas y los getters and setters de cada uno.

--- src/Balsa.java
+++ src/Balsa.java
@@ -2,10 +2,37 @@ public class Balsa {
 
     private int capacidad;
     private int tiempo;
+    private String nombre;
+
+    public Balsa(int capacidad, String nombre, int tiempo) {
+        this.capacidad = capacidad;
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+    }
 
     public Balsa(int capacidad, int tiempo) {
         this.capacidad = capacidad;
         this.tiempo = tiempo;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getCapacidad() {
