03a250b6579a4dbea7b34051748391fc162bde03
Raul <raul@gmail.com>
2025-11-24 13:08:54 +0100

    En rescate añadimos Balsa y Barco para utilizar ambas clases cuando se ejecute el Rescate. En el método run primero comprobamos si hay pasajeros, luego creamos un ArrayList para que almacene todos los pasajeros rescatados. En el bucle For añadimos los pasajeros rescatados al ArrayList. Para terminar un If que nos muestre las balsas con los pasajeros totales rescatados y un ForEach que muestre todos los pasajeros de rescatados. En el primer bucle For rescatamos primero a los pasajeros con Prioridad 1.

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,8 +1,53 @@
-public class Rescate implements Runnable{
+import java.util.ArrayList;
 
-    public Rescate(){}
+public class Rescate implements Runnable{
+    private Balsa balsa;
+    private Barco barco;
+
+    public Rescate(){
+
+    }
+
+    public Rescate(Balsa balsa, Barco barco) {
+        this.balsa = balsa;
+        this.barco = barco;
+    }
+
     @Override
     public void run() {
-        while(true){}
+        while (barco.hayPasajeros()) {
+            try {
+                Thread.sleep((long) (balsa.getTiempo() * 1000));
+
+                ArrayList<Pasajero> rescatadosEnViaje = new ArrayList<>();
+                int capacidadActual = 0;
+
+
+                for (int i = 1; i <= 4; i++) {
+                    for (int j = 0; j < barco.getPasajeros().size(); j++) {
+                        Pasajero p = barco.getPasajeros().get(j);
+                        if (capacidadActual < balsa.getCapacidad() && p.getPrioridad() == i) {
+                            rescatadosEnViaje.add(p);
+
+                            capacidadActual++;
+
+                            barco.getPasajeros().remove(p);
+                        }
+                    }
+                }
+
+                if (!rescatadosEnViaje.isEmpty()) {
+                    System.out.print("Balsa " + balsa.getNombre() + ", rescató: \n");
+                    for (Pasajero p : rescatadosEnViaje) {
+                        System.out.print("ID:" + p.getId() + "(P" + p.getPrioridad() + ") \n");
+
+                    }
+                    System.out.println("");
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
